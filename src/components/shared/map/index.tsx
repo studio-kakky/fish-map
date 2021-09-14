2cae232be2d02d5d1954a2502207d4f76a2a76ef
柿内暢昌 <nob@studio-kakky.com>
2021-09-15 00:43:39 +0900

    base mapをダークに

--- src/components/shared/map/index.tsx
+++ src/components/shared/map/index.tsx
@@ -1,7 +1,7 @@
 import DeckGL from '@deck.gl/react';
 import { StaticMap } from 'react-map-gl';
+import { MapboxOptions } from 'mapbox-gl';
 
-// Viewport settings
 const INITIAL_VIEW_STATE = {
   longitude: 139.6399642,
   latitude: 35.4119603,
@@ -9,11 +9,18 @@ const INITIAL_VIEW_STATE = {
   pitch: 0,
   bearing: 0,
 };
+const options = {
+  style: 'mapbox://styles/mapbox/dark-v10',
+} as MapboxOptions;
 
 export default function Map(): JSX.Element {
   return (
     <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true}>
-      <StaticMap mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN} reuseMaps={true} />
+      <StaticMap
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        reuseMaps={true}
+        mapOptions={options}
+      />
     </DeckGL>
   );
 }
