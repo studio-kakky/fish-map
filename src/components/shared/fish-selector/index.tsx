b2046ef770ff77ed0624a0d14b93f665db5d10c6
柿内暢昌 <nob@studio-kakky.com>
2021-09-30 16:06:39 +0900

    開閉を追加

--- src/components/shared/fish-selector/index.tsx
+++ src/components/shared/fish-selector/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { FishSelectorViewModel, makeViewModels } from './view-model';
 import { fishTypes } from '../../../_data/fish-types';
 
@@ -7,31 +8,51 @@ interface FishSelectorProps {
 
 const Component = (
   mainItems: FishSelectorViewModel[],
-  subItems: FishSelectorViewModel[]
+  subItems: FishSelectorViewModel[],
+  isOpened: boolean,
+  onClickToggle: () => void
 ): JSX.Element => {
   return (
-    <div className='fixed bottom-0 w-full box-border bg-white bg-opacity-90'>
-      <div className='flex w-full justify-between'>
-        {mainItems.map((vm) => (
-          <button
-            key={vm.typeName}
-            className='flex w-1/3 py-2 justify-center items-center font-bold text-xl border-white border-r border-b last:border-r-0'
-          >
-            {vm.label}
-          </button>
-        ))}
-      </div>
-      <div className='flex flex-wrap w-full justify-between'>
-        {subItems.map((vm, idx) => (
-          <button
-            key={vm.typeName}
-            className={`flex w-1/4 py-2 justify-center items-center font-bold text-xl border-white border-r border-b ${
-              idx % 4 === 3 && 'border-r-0'
-            }`}
-          >
-            {vm.label}
-          </button>
-        ))}
+    <div className='fixed w-full overflow-hidden'>
+      <button
+        className='flex align-middle justify-center w-full bg-white bg-opacity-60'
+        onClick={onClickToggle}
+      >
+        <span
+          className={`material-icons text-xl font-bold text-white ${
+            isOpened ? 'transform rotate-180' : ''
+          }`}
+        >
+          expand_more
+        </span>
+      </button>
+      <div
+        className={`box-border bg-white bg-opacity-90 transition-max-height duration-500 ease-in-out ${
+          !isOpened ? 'max-h-11' : 'max-h-72'
+        }`}
+      >
+        <div className='flex w-full justify-between'>
+          {mainItems.map((vm) => (
+            <button
+              key={vm.typeName}
+              className='flex w-1/3 py-2 justify-center items-center font-bold text-xl border-white border-r border-b last:border-r-0'
+            >
+              {vm.label}
+            </button>
+          ))}
+        </div>
+        <div className='flex flex-wrap w-full justify-between'>
+          {subItems.map((vm, idx) => (
+            <button
+              key={vm.typeName}
+              className={`flex w-1/4 py-2 justify-center items-center font-bold border-white border-r border-b ${
+                idx % 4 === 3 && 'border-r-0'
+              }`}
+            >
+              {vm.label}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
@@ -40,5 +61,12 @@ const Component = (
 export default function FishSelector(): JSX.Element {
   const mainItems = makeViewModels(fishTypes.slice(0, 3));
   const subItems = makeViewModels(fishTypes.slice(3, -1));
-  return Component(mainItems, subItems);
+
+  const [isOpened, setIsOpened] = useState(true);
+
+  const onClickToggle = () => {
+    setIsOpened(!isOpened);
+  };
+
+  return Component(mainItems, subItems, isOpened, onClickToggle);
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link href='https://fonts.googleapis.com/css2?family=Material+Icons' rel='stylesheet' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;300;500;700;800&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
