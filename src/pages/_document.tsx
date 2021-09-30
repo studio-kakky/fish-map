65b4b5e05f762f64fd87d22e5156d0e3ca213209
柿内暢昌 <nob@studio-kakky.com>
2021-09-30 16:12:25 +0900

    フォントの読み込みを_documentで行う

---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+
+class Document extends NextDocument {
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <meta name='robots' content='noindex' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Material+Icons&display=swap'
+            rel='stylesheet'
+          />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;300;500;700;800&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default Document;
