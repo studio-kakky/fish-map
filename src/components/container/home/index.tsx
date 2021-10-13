595900505d6a6c32a3c6aa757f12ceae832ba93d
柿内暢昌 <nob@studio-kakky.com>
2021-10-13 15:15:29 +0900

    ログインしてない場合はリダイレクト

--- src/components/container/home/index.tsx
+++ src/components/container/home/index.tsx
@@ -1,21 +1,11 @@
-import { useEffect, useState } from 'react';
-
 import Layout from '../../shared/layout';
 import Map from '../../shared/map';
-import { getCurrentPosition } from '../../../shared/libs/get-current-position';
-import { CurrentPosition } from '../../../shared/models/current-position/current-position';
 import FishSelector from '../../shared/fish-selector';
 
 export default function HomeContainer(): JSX.Element {
-  const [current, setCurrent] = useState<CurrentPosition>();
-  useEffect(() => {
-    getCurrentPosition().then((position) => {
-      setCurrent(position);
-    });
-  }, []);
   return (
     <Layout>
-      <Map current={current} />
+      <Map />
       <FishSelector />
     </Layout>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
 import type { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
+import { useEffect } from 'react';
+import { userInteractor } from '../shared/interactors/user';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
+  useEffect(() => {
+    if (router.pathname === '/sign-in') {
+      return;
+    }
+
+    userInteractor.isLoggedIn().then((v) => {
+      if (!v) {
+        router.replace('/sign-in');
+      }
+    });
+  }, [router.pathname]);
+
   return <Component {...pageProps} />;
 }
 export default MyApp;
